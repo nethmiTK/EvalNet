9add3eb9f35dc8c470a6301fa02d0b4272e5c8f0
nethmiTK <nethmitk33@gmail.com>
2025-10-18 19:54:10 +0530

    frontend setup with nextJS

---
+++ evalNet/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    < >
+      <h1 className="text-4xl font-bold">Welcome to EvalNet</h1>
+      <p className="mt-4 text-lg">Your one-stop solution for all evaluation needs.</p>
+    </>
+  );
+}
