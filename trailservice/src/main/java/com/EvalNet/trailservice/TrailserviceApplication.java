8839e1717a3bb1b1fee66dcf58fe9bad4266e5ae
nethmiTK <nethmitk33@gmail.com>
2025-10-18 18:48:57 +0530

    backend setup with spring boot

---
+++ trailservice/src/main/java/com/EvalNet/trailservice/TrailserviceApplication.java
@@ -0,0 +1,13 @@
+package com.EvalNet.trailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailserviceApplication.class, args);
+	}
+
+}
---
+++ trailservice/src/test/java/com/EvalNet/trailservice/TrailserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EvalNet.trailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
